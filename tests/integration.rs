2819b300f16029a14e220b49e242bc9054e3c0f6
aksh1618 <aakarshituppal@gmail.com>
2026-01-10 02:58:16 +0530

    fix warns

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,10 +1,11 @@
+use assert_cmd::cargo::cargo_bin_cmd;
 use assert_cmd::Command;
 use predicates::prelude::*;
 use std::io::Write;
 use tempfile::NamedTempFile;
 
 fn jsondiff() -> Command {
-    Command::cargo_bin("jsondiff").unwrap()
+    cargo_bin_cmd!("jsondiff")
 }
 
 // ============================================================================
